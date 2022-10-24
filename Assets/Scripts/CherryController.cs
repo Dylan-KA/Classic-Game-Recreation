9a1f56fbbf8a34232b030952b6c079029cf01bda
Dylan-KA <111679436+Dylan-KA@users.noreply.github.com>
2022-10-25 10:55:56 +1100

    Finished CherryController
    Cherries spawn every 10 secs at random location and destroy at their destination.

---
+++ Assets/Scripts/CherryController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CherryController : MonoBehaviour
+{
+    [SerializeField] private GameObject cherry;
+    GameObject currentCherry;
+    private Tweener tweener;
+    private Vector3 spawnPosition;
+    private Vector3 endPosition;
+    private float x = 0;
+    private float y = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tweener = gameObject.GetComponent<Tweener>();
+        InvokeRepeating("instantiate", 0.0f, 10.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentCherry != null && currentCherry.transform.position == endPosition)
+        {
+            GameObject.Destroy(currentCherry);
+        }
+    }
+
+    private void instantiate()
+    {
+        float startAxis = Random.Range(0.0f, 1.0f);
+        if (startAxis >= 0.5f)
+        {
+            float vertical = Random.Range(0.0f, 1.0f);
+            if (vertical >= 0.5f)
+            {   //start at top with random x
+                y = Camera.main.scaledPixelHeight + 16.0f;
+                x = Camera.main.scaledPixelWidth;
+                x += Random.Range(Camera.main.scaledPixelWidth * -1, 0.0f);
+                endPosition = new Vector3(x, y - (Camera.main.scaledPixelHeight + 32.0f), 0.0f);
+            }
+            else
+            {   //start at bottom with random x
+                y = Camera.main.scaledPixelHeight - 16.0f;
+                y -= Camera.main.scaledPixelHeight;
+                x = Camera.main.scaledPixelWidth;
+                x += Random.Range(Camera.main.scaledPixelWidth * -1, 0.0f);
+                endPosition = new Vector3(x, y + (Camera.main.scaledPixelHeight + 32.0f), 0.0f);
+            }
+        } else {
+            float horizontal = Random.Range(0.0f, 1.0f);
+            if (horizontal >= 0.5f)
+            {   //start at right with random y
+                x = Camera.main.scaledPixelWidth + 16.0f;
+                y = Camera.main.scaledPixelHeight;
+                y += Random.Range(Camera.main.scaledPixelHeight * -1, 0.0f);
+                endPosition = new Vector3(x - (Camera.main.scaledPixelWidth + 32.0f), y, 0.0f);
+            }
+            else
+            {   //start at left with random y
+                x = Camera.main.scaledPixelWidth - 16.0f;
+                x -= Camera.main.scaledPixelWidth;
+                y = Camera.main.scaledPixelHeight;
+                y += Random.Range(Camera.main.scaledPixelHeight * -1, 0.0f);
+                endPosition = new Vector3(x + (Camera.main.scaledPixelWidth + 32.0f), y, 0.0f);
+            }
+        }
+        spawnPosition = new Vector3(x, y, 0.0f);
+        spawnPosition = Camera.main.ScreenToWorldPoint(spawnPosition);
+        spawnPosition = new Vector3(spawnPosition.x, spawnPosition.y, 0.0f);
+        endPosition = Camera.main.ScreenToWorldPoint(endPosition);
+        endPosition = new Vector3(endPosition.x, endPosition.y, 0.0f);
+        currentCherry = (GameObject)Instantiate(cherry, spawnPosition, Quaternion.identity);
+        tweener.AddTween(currentCherry.transform, currentCherry.transform.position, endPosition, 3.0f);
+    }
+}
--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -11,14 +11,27 @@ public class PacStudentController : MonoBehaviour
     private KeyCode lastInput;
     private Tweener tweener;
     private GameObject[] obstacleSprites;
+    private GameObject[] pelletSprites;
+    private GameObject[] powerPelletSprites;
     private Vector3 futurePos;
+    private Animator pacStudentAnim;
+    private AudioSource movementAudio;
+    private AudioSource pelletAudio;
+    private GameObject particles;
 
     // Start is called before the first frame update
     void Start()
     {
         tweener = gameObject.GetComponent<Tweener>();
         obstacleSprites = GameObject.FindGameObjectsWithTag("obstacle");
+        pelletSprites = GameObject.FindGameObjectsWithTag("InitialLayout");
+        powerPelletSprites = GameObject.FindGameObjectsWithTag("PowerPellet");
         futurePos = transform.position;
+        pacStudentAnim = gameObject.GetComponent<Animator>();
+        movementAudio = GameObject.FindGameObjectWithTag("Movement").GetComponent<AudioSource>();
+        pelletAudio = GameObject.FindGameObjectWithTag("Pellet").GetComponent<AudioSource>();
+        particles = GameObject.FindGameObjectWithTag("Particle");
+        particles.SetActive(false);
     }
 
     // Update is called once per frame
@@ -27,35 +40,100 @@ public class PacStudentController : MonoBehaviour
         getInput();
         if (!tweener.TweenExists(transform)) //if not lerping
         {
+            StartCoroutine(removeParticles());
             if (lastInput != KeyCode.None)
             {
+                setFuturePos(lastInput);
                 if (walkable())
                 {
-                    setFuturePos();
+                    currentInput = lastInput;
                     tweener.AddTween(transform, transform.position, futurePos, 0.3f);
+                    rotateAnim(lastInput);
+                    playAudio();
                 } else
                 {
-                    //check currentInput and try to move
+                    setFuturePos(currentInput);
+                    if (walkable())
+                    {
+                        tweener.AddTween(transform, transform.position, futurePos, 0.3f);
+                        rotateAnim(currentInput);
+                        playAudio();
+                    }
                 }
             }
+        } else
+        {
+            particles.SetActive(true);
+            Debug.Log("true");
         }
     }
 
-    private void setFuturePos()
+    private void playAudio()
     {
-        if (lastInput == KeyCode.W)
+        Boolean pellet = false;
+        foreach (GameObject sprite in pelletSprites)
+        {
+            if (sprite.transform.position == futurePos)
+            {
+                pellet = true;
+            }
+        }
+        if (pellet)
+        {
+            pelletAudio.Play();
+        } else
+        {
+            movementAudio.Play();
+        }
+    }
+
+    private void rotateAnim(KeyCode input)
+    {
+        if (input == KeyCode.W)
+        {
+            pacStudentAnim.SetBool("BoolUp", true);
+            pacStudentAnim.SetBool("BoolDown", false);
+            pacStudentAnim.SetBool("BoolLeft", false);
+            pacStudentAnim.SetBool("BoolRight", false);
+        }
+        if (input == KeyCode.S)
+        {
+            pacStudentAnim.SetBool("BoolUp", false);
+            pacStudentAnim.SetBool("BoolDown", true);
+            pacStudentAnim.SetBool("BoolLeft", false);
+            pacStudentAnim.SetBool("BoolRight", false);
+        }
+        if (input == KeyCode.A)
+        {
+            pacStudentAnim.SetBool("BoolUp", false);
+            pacStudentAnim.SetBool("BoolDown", false);
+            pacStudentAnim.SetBool("BoolLeft", true);
+            pacStudentAnim.SetBool("BoolRight", false);
+        }
+        if (input == KeyCode.D)
+        {
+            pacStudentAnim.SetBool("BoolUp", false);
+            pacStudentAnim.SetBool("BoolDown", false);
+            pacStudentAnim.SetBool("BoolLeft", false);
+            pacStudentAnim.SetBool("BoolRight", true);
+        }
+    }
+
+    private void setFuturePos(KeyCode input)
+    {
+        if (input == KeyCode.W)
         {
             futurePos = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
         }
-        if (lastInput == KeyCode.S)
+        if (input == KeyCode.S)
         {
             futurePos = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
         }
-        if (lastInput == KeyCode.A)
+        if (input == KeyCode.A)
         {
             futurePos = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
         }
-        if (lastInput == KeyCode.D)
+        if (input == KeyCode.D)
         {
             futurePos = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
         }
@@ -83,6 +161,23 @@ public class PacStudentController : MonoBehaviour
 
     private bool walkable()
     {
+        foreach (GameObject sprite in obstacleSprites)
+        {
+            if (sprite.transform.position == futurePos)
+            {
+                return false;
+            }
+        }
         return true;
+    }
+
+    IEnumerator removeParticles()
+    {
+        Vector3 currentPos = transform.position;
+        yield return new WaitForSeconds(0.2f);
+        if (transform.position == currentPos)
+        {
+            particles.SetActive(false);
+        }
     }
 }
